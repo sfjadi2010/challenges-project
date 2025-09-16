6cef70b168e66f5e1fca12b476c1d0e7499a55b4
Salman Farsi <sfjadi2010@live.com>
2025-09-16 17:38:22 -0400

    feat: implement ToDoList component with task addition functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Greeting from "@components/basic/Greeting";
 import ClickCounter from "@components/basic/ClickCounter";
 import ToggleDetails from "@components/basic/ToggleDetails";
+import ToDoList from "@components/basic/ToDoList";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Greeting name="Alice!" message="Hello, " />
       <ClickCounter />
       <ToggleDetails />
+      <ToDoList />
     </>
   );
 }
--- src/components/basic/ToDoList.jsx
+++ src/components/basic/ToDoList.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+
+const todoList = [];
+
+const handleAddItem = (item) => {
+  todoList.push(item);
+};
 
 const ToDoList = () => {
-  return (
-    <div>ToDoList</div>
-  )
-}
+  const [newTask, setNewTask] = useState("");
 
-export default ToDoList
+  return (
+    <div className="container flex flex-col items-center justify-center p-5 m-5 border border-gray-300 rounded-lg shadow-md">
+      <h2 className="pb-5 text-2xl font-bold">To-Do List</h2>
+      <input
+        type="text"
+        placeholder="Add a new task"
+        className="border p-2 rounded-lg mb-4 w-64"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          handleAddItem(newTask);
+          setNewTask("");
+        }}
+        className="mb-4 p-2 bg-blue-500 text-white rounded-lg"
+      >
+        Add Task
+      </button>
+      <ul className="list-disc list-inside">
+        {todoList.map((item, index) => (
+          <li key={index} className="text-white">
+            {item}
+            <button className="ml-4 text-red-500 hover:text-red-700 hover:bg-amber-50 pl-2 pr-2 rounded">X</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ToDoList;
